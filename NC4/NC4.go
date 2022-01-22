576d989144ecb9f2c04cf18946b0e183ec4fae6b
YuYoung <2901161079@qq.com>
2022-01-22 21:33:23 +0800

    add(NC4): add NC4.go and tests

---
+++ NC4/NC4.go
@@ -0,0 +1,23 @@
+package NC4
+
+/**
+判断链表是否有环
+*/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	fast := head
+	slow := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
