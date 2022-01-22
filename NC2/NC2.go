2bbcff98d12e70da62d46f31d0950667a4467658
YuYoung <2901161079@qq.com>
2022-01-22 17:35:38 +0800

    fix(NC2.go): review the code and add comment

--- NC2/NC2.go
+++ NC2/NC2.go
@@ -16,12 +16,13 @@ type ListNode struct {
  */
 func reorderList(head *ListNode) {
 	// write code here
-	// 快慢指针确认中点
 	fast := head
 	slow := head
-	if fast == nil || fast.Next == nil {
+	// 确认至少有一个节点
+	if head == nil || head.Next == nil {
 		return
 	}
+	// 快慢指针确认中点
 	for fast.Next != nil {
 		fast = fast.Next
 		if fast.Next == nil {
@@ -31,26 +32,28 @@ func reorderList(head *ListNode) {
 		slow = slow.Next
 	}
 	// 颠倒后段链表
-	end := fast         // 记录旧链表的结束即新链表开始的位置
+	end := fast         // 记录旧链表的结束即新链表开始的位置，用于下一步插入到前面
 	newEnd := slow.Next // 记录新链表的结束位置
 	for newEnd.Next != nil {
+		// 提取出后个
 		temp := newEnd.Next
 		newEnd.Next = newEnd.Next.Next
-
+		// 插入到前面，slow的下一个，slow不用移动
 		temp.Next = slow.Next
 		slow.Next = temp
 	}
 	// 把后段插入到前面
 	start := head
 	for end != nil {
+		// 临时存放下一个需要取出的节点
 		temp := end.Next
-
+		// 取出颠倒的链表的首个节点-从原链表移除-插入前面链表
 		slow.Next = slow.Next.Next
-
 		end.Next = start.Next
 		start.Next = end
-
+		// 确定下一个需要插入的位置
 		start = start.Next.Next
+
 		end = temp
 	}
 }
