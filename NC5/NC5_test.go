dd17ebffe821855719cfed2743f70b249bcff8ef
YuYoung <2901161079@qq.com>
2022-01-25 12:26:38 +0800

    add(NC5): add NC5.go and tests

--- NC5/NC5_test.go
+++ NC5/NC5_test.go
@@ -1,34 +1,21 @@
 package NC5
 
-import (
-	"strconv"
-	"testing"
-)
+import "testing"
 
-/**
-根据字符串生成树，按照节点依次生成，空节点用’#‘表示
-*/
-func generateTree(string2 []string) *TreeNode {
-	const skip = -8288
-	if string2[0][0] == '#' {
-		return nil
-	}
-	// 将数字字符串数组转化为数字数组，再转化成节点数组
-	var nodes []int
-	for _, str := range string2 {
-		num, err := strconv.Atoi(str)
-		if err != nil {
-			// 非int则做一个特殊标记，在之后生成树时给予跳过
-			num = skip
-		}
-		nodes = append(nodes, num)
-	}
+func generateTree(string2 string) *TreeNode {
+	// 将数字字符串转化为数字数组，再转化成节点数组
+
+	nodes := []byte(string2)
 	var tree []*TreeNode
 	// 空占首节点。方便后续链接
-	tree = append(tree, new(TreeNode))
-	for _, num := range nodes {
+	tree = append(tree, &TreeNode{
+		Val:   0,
+		Left:  nil,
+		Right: nil,
+	})
+	for _, char := range nodes {
 		node := &TreeNode{
-			Val:   num,
+			Val:   int(char - '0'),
 			Left:  nil,
 			Right: nil,
 		}
@@ -36,10 +23,10 @@ func generateTree(string2 []string) *TreeNode {
 	}
 	length := len(tree)
 	for i := 1; i < length; i++ {
-		if 2*i < length && tree[2*i].Val != skip {
+		if 2*i < length {
 			tree[i].Left = tree[2*i]
 		}
-		if 2*i+1 < length && tree[2*i+1].Val != skip {
+		if 2*i+1 < length {
 			tree[i].Right = tree[2*i+1]
 		}
 	}
@@ -49,13 +36,13 @@ func generateTree(string2 []string) *TreeNode {
 func Test_DLR(t *testing.T) {
 	cases := []struct {
 		Name     string
-		Nodes    []string
+		Nodes    string
 		Expected int
 	}{
-		{"normal", []string{"1", "2", "3"}, 25},
-		{"long", []string{"1", "2", "0", "3", "4"}, 257},
-		{"broaden", []string{"1", "0"}, 10},
-		{"empty", []string{"#"}, 0},
+		{"normal", "123", 25},
+		{"long", "12034", 257},
+		{"broaden", "10", 10},
+		{"empty", "0", 0},
 	}
 	for _, c := range cases {
 		t.Run(c.Name, func(t *testing.T) {
